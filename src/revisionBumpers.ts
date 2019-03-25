3d70829754dda4db75271e553752e23285dd0404
Ruben Grimm <ruben.grimm@zweitag.de>
2019-03-25 23:19:18 +0100

    Refactor library to save state revision for each component

---
+++ src/revisionBumpers.ts
@@ -0,0 +1,41 @@
+import { Namespace } from '.'
+
+export type RevisionBumper = () => void
+
+interface RevisionBumpers {
+  [key: string]: RevisionBumper[];
+}
+
+const revisionBumpers: RevisionBumpers = {}
+
+function getNamespaceBumpers (namespace: Namespace): RevisionBumper[] {
+  return revisionBumpers[namespace.toString()] || []
+}
+
+function setNamespaceBumpers (namespace: Namespace, namespaceBumpers: RevisionBumper[]): void {
+  revisionBumpers[namespace.toString()] = namespaceBumpers
+}
+
+export function addBumper (namespace: Namespace, bumper: RevisionBumper): void {
+  const namespaceBumpers = getNamespaceBumpers(namespace)
+  setNamespaceBumpers(namespace, [...namespaceBumpers, bumper])
+}
+
+export function removeBumper (namespace: Namespace, bump: RevisionBumper): void {
+  const namespaceBumpers = getNamespaceBumpers(namespace)
+  setNamespaceBumpers(namespace, namespaceBumpers.filter(b => b !== bump))
+}
+
+export function hasRevisionBumper (namespace: Namespace, bumper: RevisionBumper): boolean {
+  return getNamespaceBumpers(namespace).includes(bumper)
+}
+
+export function bumpNamespace (namespace: Namespace): void {
+  getNamespaceBumpers(namespace).forEach((bump: RevisionBumper) => bump())
+}
+
+export function resetBumpers (): void {
+  Object.keys(revisionBumpers).forEach(key => {
+    delete revisionBumpers[key]
+  })
+}
--- src/useConnectedReducer.ts
+++ src/useConnectedReducer.ts
@@ -1,28 +1,49 @@
 import {
-  ConnectedDispatch,
-  Namespace,
-  addDispatcher,
-  dispatchNamespace,
-  removeDispatcher
-} from './dispatchers'
+  Dispatch,
+  buildDispatch
+} from './buildDispatch'
 import {
   Reducer,
   useEffect,
-  useReducer
+  useState
 } from 'react'
+import {
+  addBumper,
+  hasRevisionBumper,
+  removeBumper
+} from './revisionBumpers'
+import {
+  getNamespaceState,
+  hasNamespaceState,
+  setNamespaceState
+} from './states'
+import { Namespace } from '.'
+
+function initializeNamespaceState (namespace: Namespace, initialState: any): void {
+  if (!hasNamespaceState(namespace)) {
+    setNamespaceState(namespace, initialState)
+  }
+}
+
+function useRevisionBumper (namespace: Namespace): void {
+  const [revision, setRevision] = useState(0)
+  const revisionBumper = (): void => setRevision(revision + 1)
+
+  if (!hasRevisionBumper(namespace, revisionBumper)) {
+    addBumper(namespace, revisionBumper)
+  }
+  useEffect(() => {
+    return () => removeBumper(namespace, revisionBumper)
+  })
+}
 
 export default function useConnectedReducer (
   namespace: Namespace,
   reducer: Reducer<any, any>,
   initialState: any
-): [any, ConnectedDispatch] {
-  const [state, dispatch] = useReducer(reducer, initialState)
+): [any, Dispatch] {
+  initializeNamespaceState(namespace, initialState)
+  useRevisionBumper(namespace)
 
-  useEffect(() => {
-    addDispatcher(namespace, dispatch)
-
-    return () => removeDispatcher(namespace, dispatch)
-  })
-
-  return [state, dispatchNamespace(namespace)]
+  return [getNamespaceState(namespace), buildDispatch(namespace, reducer)]
 }
