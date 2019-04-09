8e1f30008f941134d85e9fc0d72a413e96ac4d48
Ruben Grimm <ruben.grimm@zweitag.de>
2019-04-10 00:56:19 +0200

    Use ConnectedProvider to store namespaced states

--- src/ConnectedProvider.tsx
+++ src/ConnectedProvider.tsx
@@ -1,57 +1,62 @@
 import React, {
-  Context,
   ReactElement,
   createContext,
   useState
 } from 'react'
-import ContextsProvider from './ContextsProvider'
 import { Namespace } from '.'
 
-interface ConnectedContextInterface {
-  getContext: (contextName: Namespace) => Context<any> | null;
-  setContext: (contextName: Namespace, context: Context<any>) => void;
+interface NamespaceStates {
+  [key: string]: any;
 }
 
-const ConnectedContext = createContext<ConnectedContextInterface>({
-  getContext: () => null,
-  setContext: () => { }
-})
+type ConnectedContext = {
+  getNamespaceState: (namespace: Namespace, state: any) => any;
+  setNamespaceState: (namespace: Namespace, state: any) => void;
+} | undefined
+
+export const ConnectedContext = createContext<ConnectedContext>(undefined)
 
 interface ConnectedProviderProps {
-  children: ReactElement;
+  children?: any;
 }
 
 function ConnectedProvider ({ children }: ConnectedProviderProps): ReactElement {
-  const [contexts, setContexts] = useState<ConnectedContextState>({})
-
-  const getContext = (
-    contextName: Namespace
-  ): Context<any> => contexts[contextName.toString()]
-
-  const setContext = (
-    contextName: Namespace,
-    context: Context<any>
-  ): void => setContexts({
-    ...contexts,
-    [contextName.toString()]: context
+  const [namespaceStates, setNamespaceStates] = useState<NamespaceStates>({})
+  const [connectedContext] = useState<ConnectedContext>({
+    getNamespaceState: () => null,
+    setNamespaceState: () => null
   })
 
-  const connectedContext = {
-    getContext,
-    setContext
+  if (!connectedContext) {
+    throw new Error()
+  }
+
+  connectedContext.getNamespaceState = (namespace: Namespace, initialState: any) => {
+    const ns = namespace.toString()
+    if (namespaceStates.hasOwnProperty(ns)) {
+      return namespaceStates[ns]
+    } else {
+      return initialState
+    }
+  }
+
+  connectedContext.setNamespaceState = (namespace: Namespace, state: any) => {
+    const ns = namespace.toString()
+    if (state === namespaceStates[ns]) {
+      return
+    }
+
+    setNamespaceStates({
+      ...namespaceStates,
+      [ns]: state
+    })
   }
 
   return (
     <ConnectedContext.Provider value={connectedContext}>
-      <ContextsProvider contexts={contexts}>
-        {children}
-      </ContextsProvider>
+      {children}
     </ConnectedContext.Provider>
   )
-}
-
-export interface ConnectedContextState {
-  [key: string]: Context<any>;
 }
 
 export default ConnectedProvider
--- src/revisionBumpers.ts
+++ src/revisionBumpers.ts
@@ -1,6 +1,6 @@
 import { Namespace } from '.'
 
-type RevisionBumper = () => void
+export type RevisionBumper = () => void
 
 interface RevisionBumpers {
   [key: string]: RevisionBumper[];
@@ -26,16 +26,6 @@ export function removeBumper (namespace: Namespace, bump: RevisionBumper): void 
   setNamespaceBumpers(namespace, namespaceBumpers.filter(b => b !== bump))
 }
 
-export function hasBumper (namespace: Namespace, bumper: RevisionBumper): boolean {
-  return getNamespaceBumpers(namespace).includes(bumper)
-}
-
 export function bumpNamespace (namespace: Namespace): void {
   getNamespaceBumpers(namespace).forEach((bump: RevisionBumper) => bump())
-}
-
-export function resetBumpers (): void {
-  Object.keys(revisionBumpers).forEach(key => {
-    delete revisionBumpers[key]
-  })
 }
