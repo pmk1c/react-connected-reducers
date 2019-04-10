11059c2efc857b31042a58342c87d4c9404c66ab
Ruben Grimm <ruben.grimm@zweitag.de>
2019-04-10 18:43:24 +0200

    Fix tests and add changelog entry

--- test/useConnectedReducer.test.tsx
+++ test/useConnectedReducer.test.tsx
@@ -1,49 +1,55 @@
+import {
+  ConnectedProvider,
+  useConnectedReducer
+} from '../src'
 import React, {
-  ReactElement,
-  useEffect
+  Dispatch,
+  ReactElement
 } from 'react'
 import ReactTestRenderer from 'react-test-renderer'
 import { expect } from 'chai'
-import { resetBumpers } from '../src/revisionBumpers'
-import { resetStates } from '../src/states'
-import { useConnectedReducer } from '../src'
 
 describe('useConnectedReducer', () => {
-  afterEach(() => {
-    resetBumpers()
-    resetStates()
-  })
+  let dispatchAction = (): void => { }
+  let useTestReducer = (): [any, Dispatch<any>] => useConnectedReducer(
+    'reducer',
+    (state: string, action: string): string => {
+      return action === 'action' ? 'newState' : state
+    },
+    'state'
+  )
+  let ComponentWithStateAndDispatch = (): ReactElement => {
+    const [state, dispatch] = useTestReducer()
+    dispatchAction = () => dispatch('action')
+    return <>{state}</>
+  }
+  let ComponentWithState = (): ReactElement => {
+    const [state] = useTestReducer()
+    return <>{state}</>
+  }
 
   describe('single component', () => {
     it('returns initial state to component', () => {
-      const reducer = (state: string): string => {
-        return state
-      }
-      function Component (): ReactElement {
-        const [state] = useConnectedReducer('reducer', reducer, 'state')
-        return <>{state}</>
-      }
-      const renderer = ReactTestRenderer.create(<Component />)
+      const renderer = ReactTestRenderer.create(
+        <ConnectedProvider>
+          <ComponentWithState />
+        </ConnectedProvider>
+      )
 
       expect(renderer.toJSON()).to.equal('state')
     })
 
     it('dispatches action through reducer and updates component state', () => {
-      const reducer = (state: string, action: string): string => {
-        return action === 'action' ? 'newState' : state
-      }
-      function Component (): ReactElement {
-        const [state, dispatch] = useConnectedReducer('reducer', reducer, 'state')
-        useEffect(() => {
-          dispatch('action')
-        })
-        return <>{state}</>
-      }
-      const app = <Component />
+      const app = (
+        <ConnectedProvider>
+          <ComponentWithStateAndDispatch />
+        </ConnectedProvider>
+      )
       const renderer = ReactTestRenderer.create(app)
 
       expect(renderer.toJSON()).to.equal('state')
 
+      dispatchAction()
       renderer.update(app)
 
       expect(renderer.toJSON()).to.equal('newState')
@@ -52,68 +58,17 @@ describe('useConnectedReducer', () => {
 
   describe('multiple components', () => {
     it('updates all connected components when dispatching an action', () => {
-      const useMyReducer = (): [string, (action: string) => void] => useConnectedReducer(
-        'reducer',
-        (state: string, action: string): string => {
-          return action === 'action' ? 'newState' : state
-        },
-        'state'
-      )
-      function ComponentA (): ReactElement {
-        const [state] = useMyReducer()
-        return <>{state}</>
-      }
-      function ComponentB (): ReactElement {
-        const [state, dispatch] = useMyReducer()
-        useEffect(() => {
-          dispatch('action')
-        })
-        return <>{state}</>
-      }
       const app = (
-        <>
-          <ComponentA />
-          <ComponentB />
-        </>
+        <ConnectedProvider>
+          <ComponentWithState />
+          <ComponentWithStateAndDispatch />
+        </ConnectedProvider>
       )
       const renderer = ReactTestRenderer.create(app)
 
       expect(renderer.toJSON()).to.eql(['state', 'state'])
 
-      renderer.update(app)
-
-      expect(renderer.toJSON()).to.eql(['newState', 'newState'])
-    })
-
-    it('updates all connected components when dispatching an action and dispatch happens before mount', () => {
-      const useMyReducer = (): [string, (action: string) => void] => useConnectedReducer(
-        'reducer',
-        (state: string, action: string): string => {
-          return action === 'action' ? 'newState' : state
-        },
-        'state'
-      )
-      function ComponentA (): ReactElement {
-        const [state, dispatch] = useMyReducer()
-        useEffect(() => {
-          dispatch('action')
-        })
-        return <>{state}</>
-      }
-      function ComponentB (): ReactElement {
-        const [state] = useMyReducer()
-        return <>{state}</>
-      }
-      const app = (
-        <>
-          <ComponentA />
-          <ComponentB />
-        </>
-      )
-      const renderer = ReactTestRenderer.create(app)
-
-      expect(renderer.toJSON()).to.eql(['state', 'state'])
-
+      dispatchAction()
       renderer.update(app)
 
       expect(renderer.toJSON()).to.eql(['newState', 'newState'])
